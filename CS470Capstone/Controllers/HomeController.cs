39eceab874a368507963968a3b9ff43f648f0a09
CC\MISKULF <fmiskuly19@jcu.edu>
2018-09-17 16:18:13 -0400

    removed example files we arent using anymore. removed menu links to now non existent pages

--- CS470Capstone/Controllers/HomeController.cs
+++ CS470Capstone/Controllers/HomeController.cs
@@ -27,19 +27,5 @@ namespace CCFLoggingConfig.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
