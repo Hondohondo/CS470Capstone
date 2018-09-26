5ed6b210a7f2091d50554b8f5f23b757ec2c6f9c
julienkas <julienkas@gmail.com>
2018-09-26 15:42:54 -0400

    Added data table for the config page.

--- CS470Capstone/Content/scripts/Config/Config.js
+++ CS470Capstone/Content/scripts/Config/Config.js
@@ -1,9 +1,29 @@
 ﻿var config = {
     Initialize: function () {
-
+        config.InitializeConfigDataTable();
         //config.CreateConfig();
         //config.CreateSSISConfig();
     },
+
+    InitializeConfigDataTable: function () {
+        $("#config-logs").DataTable({
+            ajax: {
+                url: "../Config/GetConfigsForDataTable",
+                type: "POST",
+                datatype: "json",
+            },
+            rowId: "LogID",
+            serverSide: true,
+            searching: false,
+            pageLength: 10,
+            lengthChange: false,
+            order: [1, "desc"],
+            columns: [
+                { data: "Application", sortable: true, searchable: false, name: "Title" },
+                { Name: "Actions", sortable: false, searchable: false }
+            ],
+        });
+    }
 
     //CreateConfig: function () {
     //    configAPI.CreateConfig(function (response) {
