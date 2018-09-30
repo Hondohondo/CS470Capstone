c09a8e9c67ef79cd04900fb34992e05221f171f9
nickm24 <nickmarsic@me.com>
2018-09-30 15:28:06 -0400

    Syssssislog files js files as well as .cshtml files. Working inside Views and Content folders

--- CS470Capstone/Content/scripts/Sysssislog/sysssislog.API.js
+++ CS470Capstone/Content/scripts/Sysssislog/sysssislog.API.js
@@ -1,3 +1,20 @@
 ﻿var syssislogAPI = {
 
+    GetSysssislog: function (ID, callback) {
+        $.ajax({
+            type: "POST",
+            url: "../Sysssislog/GetSysssislog",
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            data: JSON.stringify({
+                logID: logID
+            }),
+            success: function (data) {
+                typeof callback === "function" && callback(data);
+            },
+            error: function (xhr) {
+                alert('Request Status: ' + xhr.status + ' Status Text: ' + xhr.statusText + ' ' + xhr.responseText);
+            }
+        });
+    }
 }
--- CS470Capstone/Content/scripts/Sysssislog/sysssislog.js
+++ CS470Capstone/Content/scripts/Sysssislog/sysssislog.js
@@ -1,3 +1,79 @@
 ﻿var syssislog = {
+    Initialize: function () {
+        log.InitializeLogsDataTable();
+
+        $(document).on("click", "#button-view-full-log", function () {
+            log.GetLog($(this).attr("data-log-id"));
+        });
+    },
+
+    GetSysssislog: function (ID) {
+        //open modal
+        $("#modal-log-details").modal("show");
+
+        sysssislogAPI.GetSysssislog(ID, function (response) {
+            if (response) {
+                //build table with data
+                //possible use handlebars template?
+
+                $("#log-details").addClass("hidden");
+                $("#table-sysssislog-details > tbody:last-child").empty();
+
+                var ID = "<tr><td><b> ID: </b></td><td>" + response.ID + "</td></tr>";
+                var Event = "<tr><td><b> Event: </b></td><td>" + response.Event + "</td></tr>";
+                var Computer = "<tr><td><b> Computer: </b></td><td>" + response.Computer + "</td></tr>";
+                var Operator = "<tr><td><b> Operator: </b></td><td>" + response.Operator + "</td></tr>";
+                var Source = "<tr><td><b> Source: </b></td><td>" + response.Source + "</td></tr>";
+                var SourceID = "<tr><td><b> SourceID: </b></td><td>" + response.SourceID + "</td></tr>";
+                var ExectionID = "<tr><td><b> ExecutionID: </b></td><td>" + response.ExectionID + "</td></tr>";
+                var StartTime = "<tr><td><b> StartTime: </b></td><td>" + response.StartTime + "</td></tr>";
+                var EndTime = "<tr><td><b> EndTime: </b></td><td>" + response.EndTime + "</td></tr>";
+                var DataCode = "<tr><td><b> DataCode: </b></td><td>" + response.DataCode + "</td></tr>";
+                var DataBytes = "<tr><td><b> ThreadName: </b></td><td>" + response.DataBytes + "</td></tr>";
+                var Message = "<tr><td><b> Message: </b></td><td>" + response.Message + "</td></tr>";
+
+                $("#table-sysssislog-details > tbody:last-child").append(ID + Event + Computer + Operator + Source + SourceID + ExectionID + StartTime + EndTime + DataCode + DataBytes + Message);
+                $("#sysssislog-details").removeClass("hidden");
+            }
+            else {
+                console.log(response)
+            }
+        });
+    },
+
+    InitializeLogsDataTable: function () {
+        $("#table-sysssislog").dataTable({
+            ajax: {
+                url: "../Sysssislog/GetSysssislogForDataTable",
+                type: "POST",
+                datatype: "json",
+            },
+            rowId: "ID",
+            serverSide: true,
+            searching: true,
+            pageLength: 10,
+            lengthChange: true,
+            order: [1, "desc"],
+            columns: [
+                { data: "Message", sortable: true, searchable: true, name: "Message" },
+                { data: "Event", sortable: true, searchable: false, name: "Event" },
+                { data: "Computer", sortable: true, searchable: false, name: "Computer" },
+                { data: "Operator", sortable: true, searchable: false, name: "Operator" },
+                { data: "AuthenticatedUser", sortable: true, searchable: false, name: "AuthenticatedUser" },
+            ],
+            columnDefs: [
+                {
+                    targets: 5,
+                    visible: true,
+                    render: function (data, type, row) {
+                        // data-toggle="modal" data-target="#modal-log"
+                        return '<button class="btn btn-sm btn-primary" id="button-view-full-sysssislog" data-log-id="' + row.ID + '"><span class="fa fa-edit"><span></button>'
+                    }
+                },
+            ]
+        });
+    }
 
 }
+
+$(document).ready(sysssislog.Initialize());
